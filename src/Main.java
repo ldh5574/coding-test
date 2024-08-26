1be9f238336839edc7bd0848609f33b798b78f9c
DHL <ldh5574@gmail.com>
2024-08-27 02:12:46 +0900

    통근버스 출발 순서 검증하기

--- src/Main.java
+++ src/Main.java
@@ -3,79 +3,99 @@ import java.util.*;
 
 public class Main {
 
-    public static int n, m, k;
-    public static int line[];
-    public static int v[];
-    public static int dap = Integer.MAX_VALUE;
+    public static long dap = 0;
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        line = new int[n];
-        v = new int[n];
-        for(int i=0; i<n; i++){
-            int kg = Integer.parseInt(st.nextToken());
-            line[i] = kg;
+        int arr[] = new int[n+2];
+        for(int i=1; i<n+1; i++){
+            int num = Integer.parseInt(st.nextToken());
+            arr[i] = num;
         }
 
-        dfs(new int[n], 0);
-
-        // bw.newLine();
-        bw.write(String.valueOf(dap) + "\n");
-        bw.flush();
-        bw.close();
-    }
-
-    // 첫번째 미션 : 레일 순서 정하기
-    static void dfs(int[] railOrder, int count) {
-//        for(int i=0; i<railOrder.length; i++){
-//            System.out.println(i + " " + railOrder[i] + " " + count);
+        // 답은 맞지만, 시간초과 발생
+//        for(int i=0; i<n; i++){
+//            for(int j=i+1; j<n; j++){
+//                int a = arr[i];
+//                int b = arr[j];
+//
+//                if(a>b){
+//                    continue;
+//                }
+//                // a < b
+//                for(int k=j+1; k<n; k++){
+//                    int c = arr[k];
+//                    if(a > c){
+//                        dap++;
+//                        // System.out.println(a + " " + b + " " + c);
+//                    }
+//                }
+//            }
 //        }
-//        System.out.println();
 
-        if(count == n) {    // railOrder 가 꽉 채워져서 n개의 레일의 순서가 모두 정해졌다면
-            int w = calWeight(railOrder);   // 바구니제한m과 k횟수로 최소무게 구하러 가자!
-            dap = Math.min(w, dap);
-            return;
-        }
+        // 시간초과 해결 DP 구현
+        // A > C 인 경우 계산해주기
+        // arr[0][4] = "1 보다 오른쪽에 있는 배열 값들에 대해, 4보다 작은 수들의 개수"인 0개*를 저장
+        // arr[0][3] = "3 보다 오른쪽에 있는 배열 값들에 대해, 4보다 작은 수들의 개수"인 1개를 저장
+        // arr[0][2] = "5 보다 오른쪽에 있는 배열 값들에 대해, 4보다 작은 수들의 개수"인 2개를 저장
+        // arr[0][1] = "2 보다 오른쪽에 있는 배열 값들에 대해, 4보다 작은 수들의 개수"인 2개를 저장
+        // arr[0][0] = "4 보다 오른쪽에 있는 배열 값들에 대해, 4보다 작은 수들의 개수"인 3개를 저장
+        //   4 2 5 3 1
+        // 4 3 2 2 1 0개*
+        // 2     <- <- <-
+        // 5
+        // 3
+        // 1
 
-        for(int i = 0; i < n; i++) {
-            if(v[i] == 0) {
-                v[i] = 1;
-                railOrder[count] = line[i];
-                dfs(railOrder, count+1);
-                v[i] = 0;
+        // DP 저장
+        int dp[][] = new int[n+2][n+2];
+        for(int i=1; i<=n; i++){
+            for(int j=n-1; j>=1; j--){
+                if(arr[i] > arr[j+1]){
+                    dp[i][j] = dp[i][j+1] + 1;
+                }else{
+                    dp[i][j] = dp[i][j+1];
+                }
+
             }
         }
-    }
 
-    // 두번째 미션 : 정해진 레일 순서에 대해서, 바구니제한m과 k횟수로 최소무게 합 구하기
-    static int calWeight(int[] railOrder) {
-        int sum = 0;
-        int railNum = 0;
-        for(int i = 0; i < k; i++) {    // k번 시행횟수 안에서
-            int checkWeight = 0;        // 일회당 옮겨질 무게 변수 초기화해주고
-            while(checkWeight + railOrder[railNum] <= m) {  // 무게제한m보다 작으면
-                checkWeight += railOrder[railNum];          // 더해주고
-                // railNum = (railNum + 1 == n) ? 0 : railNum+1;
-                if(railNum+1 == n){     // 현재 레일이 마지막이었다면 (=다음레일이 없다면)
-                    railNum = 0;        // 첫번째 레일로 다시 돌아가기 위해 변수 초기화
-                }else{                  // 현재 레일이 중간 레일이라면
-                    railNum++;          // 다음 레일로 가기 위한 변수 +1
+        // DP 출력
+//        for(int i=1; i<=n; i++){
+//            for(int j=1; j<=n; j++){
+//                if(arr[i] < arr[j]){
+//                    System.out.print("+" + dp[i][j]);
+//                }
+//                else{
+//                    System.out.print(" " + dp[i][j]);
+//                }
+//            }
+//            System.out.println();
+//        }
+
+        // A < B 더해주기
+        for(int i=1; i<=n; i++){
+            for(int j=i+1; j<=n; j++){
+                if(arr[i] < arr[j]){
+                    dap += dp[i][j];
+                    // System.out.print("\t" + arr[i] + "," + arr[j] + "=" + dp[i][j]);
                 }
             }
-            // 1회 안에 바구니에 담을 수 있는 무게 전부 담았다면 sum에 더해주자
-            sum += checkWeight;
+            // System.out.println();
         }
-        return sum;
+
+        bw.write(String.valueOf(dap));
+        bw.flush();
+        bw.close();
+        return;
     }
+
 }
 
 
